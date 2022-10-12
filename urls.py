97e19741306e99b5812773b57e94b652deb9bd68
zenger05 <zenger05@yandex.ru>
2022-10-13 00:19:56 +0300

    [feat] Add posts app [feat] Add group app

--- urls.py
+++ urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls', namespace='posts'))
+    path('', include('posts.urls', namespace='posts')),
+    path('auth/', include('users.urls', namespace='users')),
+    path('auth/', include('django.contrib.auth.urls'))
 ]
