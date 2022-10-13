b803d65c210bc1c1782b8677d22a73a7e19653b2
zenger05 <zenger05@yandex.ru>
2022-10-14 00:27:57 +0300

    [feat] Add posts app [feat] Add group app

--- urls.py
+++ urls.py
@@ -1,10 +1,13 @@
+from django.conf import settings
 from django.contrib import admin
 
 from django.urls import path, include
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls', namespace='posts')),
     path('auth/', include('users.urls', namespace='users')),
     path('auth/', include('django.contrib.auth.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
